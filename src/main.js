c64502ea1c07b4eac611e7f33a7be5b3fdcf121e
Ryan <rolearyprogramming@gmail.com>
2024-05-15 12:13:25 -0600

    Testcommit3

--- src/main.js
+++ src/main.js
@@ -9,10 +9,31 @@ var currentCover;
 
 // Add your event listeners here 👇
 
-var randomCover = document.getElementById("controls");
-randomCover.style.backgroundColor = "green";
+// var randomCover = document.getElementById("controls");
+// randomCover.style.backgroundColor = "green";
 
-
+function showThing() {
+  var thing = document.getElementById("controls");
+  function getRandomInt(max) { 
+  return Math.floor(Math.random() * max);}
+    if (thing.style.display ==="none"){
+      
+  thing.style.display = "block";
+  } else {
+    if (getRandomInt(5) === 3){
+      var rgb = [];
+      for (var i = 0; i < 3; i++){
+        rgb.push(Math.floor(Math.random()*255));
+        
+      }
+      thing.style.color = 'rgb('+ rgb.join(',')+')';
+    }
+    else{
+      thing.style.color = 'black';
+    }
+  thing.style.display = "none";
+  }
+  }
 // Create your event handlers and other functions here 👇
 
 
